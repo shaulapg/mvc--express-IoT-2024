e1fc25fa57f74d797f0167a68627558adecec24e
shaulapg <a01712013@tec.com>
2024-11-07 17:05:53 -0600

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,59 @@
 import dotenvFlow from "dotenv-flow";
-import unknownResource from "./middlewares/unknown-resource";
-import unknownError from "./middlewares/unknown-error";
 import express from "express";
 import studentRouter from "./routes/student";
 import testRoutes from "./routes/test";
+import unknownResource from "./middlewares/unknown-resource";
+import unknownError from "./middlewares/unknown-error";
 import validationError from "./middlewares/validation-error";
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
+import cors from "cors";
 
+//Para poder acceder a las variables del ambiente
+// Cargar dotenv-flow solo si no estamos en producción
 if (process.env.NODE_ENV !== "production") {
   dotenvFlow.config();
 }
 
-//Para poder acceder a las variables del ambiente (.env)
-dotenvFlow.config();
-
 const app = express();
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: "*", // Adjust this to your frontend's origin
+    methods: ["GET", "POST"],
+  },
+});
 
+// Middleware para CORS - Permite todas las solicitudes
+app.use(cors());
 // Middleware para parsear JSON
 app.use(express.json());
 
+// Rutas de la api
 app.use("/api/v1/student", studentRouter);
 
+// Rutas de prueba
 app.use("/error", testRoutes);
 
-//Middleware
-app.use(validationError);
-app.use(unknownResource);
+// WebSocket Connection
+io.on("connection", (socket) => {
+  console.log("A client connected:", socket.id);
 
-//Middleware de Error
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+// Make io accessible to routers/controllers
+app.set("io", io);
+
+// Middlewares
+app.use(validationError); // Error de validacion
+app.use(unknownResource); // Error 404, recurso no encontrado
+
+// Middlewares de error
 app.use(unknownError);
 
-app.listen(process.env.SERVER_PORT, function () {
+server.listen(process.env.SERVER_PORT, function () {
   console.log("Escuchando puerto " + process.env.SERVER_PORT);
 });
